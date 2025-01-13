3d659142fdffa794b9106626dc9e75e72d48c14b
Amit Sen <amit.sen@storyblok.com>
2025-01-13 18:32:04 +0100

    Initial Commit

---
+++ storyblok-ultimate-tutorial-ios/storyblok_ultimate_tutorial_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  storyblok_ultimate_tutorial_iosApp.swift
+//  storyblok-ultimate-tutorial-ios
+//
+//  Created by Amit Sen on 13.01.25.
+//
+
+import SwiftUI
+
+@main
+struct storyblok_ultimate_tutorial_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
